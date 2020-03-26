2c230a5ca8239e2b5cbb9d5295efc23eca606890
george-sarji <georgesarji@gmail.com>
2020-03-26 14:59:39 +0200

    Added base clion files

---
+++ cmake-build-debug/CMakeFiles/234124_introduction_to_system_programming.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "234124_introduction_to_system_programming.exe"
+  "234124_introduction_to_system_programming.exe.manifest"
+  "234124_introduction_to_system_programming.pdb"
+  "CMakeFiles/234124_introduction_to_system_programming.dir/Assignments/hw0/part1.c.obj"
+  "CMakeFiles/234124_introduction_to_system_programming.dir/main.c.obj"
+  "lib234124_introduction_to_system_programming.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/234124_introduction_to_system_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
