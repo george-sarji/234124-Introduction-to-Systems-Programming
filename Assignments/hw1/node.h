8c18fdd9c34b8a00a68e82e23be88f212ad9aa2e
george.sarji <georgesarji@gmail.com>
2020-05-10 14:08:30 +0300

    Implemented more functions in node

--- Assignments/hw1/node.h
+++ Assignments/hw1/node.h
@@ -6,7 +6,7 @@
 typedef struct node_t
 {
     char *key;
-    char *value;
+    char *data;
     struct node_t *next;
 } * Node;
 
@@ -14,6 +14,10 @@ void nodeDestroy(Node node);
 Node nodeCreate();
 Node nodeGetNext(Node node);
 char *nodeGetData(Node node);
+char *nodeSetData(Node node, const char *data);
+char *nodeSetKey(Node node, const char *key);
+Node nodeSetNext(Node node, Node next);
+bool nodeCompareKey(Node node, const char *key);
 
 Node nodeCreate()
 {
@@ -21,30 +25,87 @@ Node nodeCreate()
     if (node == NULL)
         return NULL;
     node->key = NULL;
-    node->value = NULL;
+    node->data = NULL;
+    node->next = NULL;
     return node;
 }
 void nodeDestroy(Node node)
 {
     if (node == NULL)
         return;
-    free(node->key);
-    free(node->value);
+    if (node->key != NULL)
+        free(node->key);
+    if (node->data != NULL)
+        free(node->data);
     free(node);
 }
 
 Node nodeGetNext(Node node)
 {
+    if (node == NULL)
+        return NULL;
     return node->next;
 }
 
 char *nodeGetKey(Node node)
 {
+    if (node == NULL)
+        return NULL;
     return node->key;
 }
 
 char *nodeGetData(Node node)
 {
-    return node->value;
+    if (node == NULL)
+        return NULL;
+    return node->data;
 }
+
+char *nodeSetKey(Node node, const char *key)
+{
+    free(node->key);
+    char *new_key = malloc(sizeof(char) * (1 + strlen(key)));
+    if (new_key == NULL)
+    {
+        node->key = NULL;
+        return NULL;
+    }
+    strcpy(new_key, key);
+    node->key = new_key;
+    return new_key;
+}
+char *nodeSetData(Node node, const char *data)
+{
+    // Allocate new data.
+    free(node->data);
+    char *new_data = malloc(sizeof(char) * (1 + strlen(data)));
+    if (new_data == NULL)
+    {
+        node->data = NULL;
+        return NULL;
+    }
+    strcpy(new_data, data);
+    node->data = new_data;
+    return new_data;
+}
+
+Node nodeSetNext(Node node, Node next)
+{
+    if (node == NULL || next == NULL)
+    {
+        return NULL;
+    }
+    node->next = next;
+    return next;
+}
+
+bool nodeCompareKey(Node node, const char *key)
+{
+    if (node == NULL || key == NULL || nodeGetKey(node) == NULL)
+    {
+        return false;
+    }
+    return strcmp(nodeGetKey(node), key) == 0;
+}
+
 #endif
