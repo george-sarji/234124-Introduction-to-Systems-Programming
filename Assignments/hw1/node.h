8966e82c971aa95ca31f29faae206a3dc0ff7965
george.sarji <georgesarji@gmail.com>
2020-05-10 12:41:20 +0300

    Updated node header file to have more functions

--- Assignments/hw1/node.h
+++ Assignments/hw1/node.h
@@ -1,16 +1,50 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <stdlib.h>
+#include <stdio.h>
 typedef struct node_t
-{
-    struct pair_t *data;
-    struct node_t *next;
-} * Node;
-
-typedef struct pair_t
 {
     char *key;
     char *value;
-} * MapPair;
+    struct node_t *next;
+} * Node;
 
+void nodeDestroy(Node node);
+Node nodeCreate();
+Node nodeGetNext(Node node);
+char *nodeGetData(Node node);
+
+Node nodeCreate()
+{
+    Node node = malloc(sizeof(*node));
+    if (node == NULL)
+        return NULL;
+    node->key = NULL;
+    node->value = NULL;
+    return node;
+}
+void nodeDestroy(Node node)
+{
+    if (node == NULL)
+        return;
+    free(node->key);
+    free(node->value);
+    free(node);
+}
+
+Node nodeGetNext(Node node)
+{
+    return node->next;
+}
+
+char *nodeGetKey(Node node)
+{
+    return node->key;
+}
+
+char *nodeGetData(Node node)
+{
+    return node->value;
+}
 #endif
