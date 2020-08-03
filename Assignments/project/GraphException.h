5dacd96f4642423dfdb59d458c72ba98bb85b8e8
george-sarji <georgesarji@gmail.com>
2020-08-03 20:25:39 +0300

    Added base graph exceptions file

---
+++ Assignments/project/GraphException.h
@@ -0,0 +1,13 @@
+#ifndef GRAPH_EXCEPTION_H
+#define GRAPH_EXCEPTION_H
+
+#include <exception>
+
+namespace mtm
+{
+    class GraphException : std::exception
+    {
+    };
+}; // namespace mtm
+
+#endif
