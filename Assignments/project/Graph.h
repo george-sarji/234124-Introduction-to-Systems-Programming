28afe7ce9565c32789f101169718e762b8492c93
george-sarji <georgesarji@gmail.com>
2020-08-03 20:17:22 +0300

    Added graph base files

---
+++ Assignments/project/Graph.h
@@ -0,0 +1,17 @@
+#include <vector>
+#include <map>
+#include <string>
+namespace mtm
+{
+
+    class Graph
+    {
+    private:
+        std::map<std::string, std::string> links;
+        std::vector<std::string> points;
+
+    public:
+        std::map<std::string, std::string> getLinks();
+        std::vector<std::string> getPoints();
+    };
+}; // namespace mtm
