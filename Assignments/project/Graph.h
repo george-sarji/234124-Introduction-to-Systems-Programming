e42b21707a8b63e06f2aae376f350a450f89882f
george-sarji <georgesarji@gmail.com>
2020-08-03 20:33:29 +0300

    Fixed extension of file, added base function declarations

--- Assignments/project/Graph.h
+++ Assignments/project/Graph.h
@@ -1,7 +1,6 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-
 #include <vector>
 #include <map>
 #include <string>
@@ -17,6 +16,10 @@ namespace mtm
     public:
         std::map<std::string, std::string> getLinks();
         std::vector<std::string> getPoints();
+        void addGraphPoint(std::string pointName);
+        void removeGraphPoint(std::string pointName);
+        void addLink(std::string to, std::string from);
+        void removeLink(std::string to, std::string from);
     };
 }; // namespace mtm
 
