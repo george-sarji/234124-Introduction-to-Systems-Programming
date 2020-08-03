4076b12e0f66c3b64386bb6f5c4cc8df756912d0
george-sarji <georgesarji@gmail.com>
2020-08-03 20:34:41 +0300

    Re-added vector for links, changed points to set

--- Assignments/project/Graph.h
+++ Assignments/project/Graph.h
@@ -4,18 +4,20 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <std>
 namespace mtm
 {
 
     class Graph
     {
     private:
-        std::map<std::string, std::string> links;
-        std::vector<std::string> points;
+        std::vector<std::map<std::string, std::string>> links;
+        std::set<std::string> points;
 
     public:
-        std::map<std::string, std::string> getLinks();
-        std::vector<std::string> getPoints();
+        Graph();
+        std::vector<std::map<std::string, std::string>> getLinks();
+        std::set<std::string> getPoints();
         void addGraphPoint(std::string pointName);
         void removeGraphPoint(std::string pointName);
         void addLink(std::string to, std::string from);
