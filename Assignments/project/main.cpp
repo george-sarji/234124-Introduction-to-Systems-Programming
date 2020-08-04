dd3366532d5de66e9b1a372f8d6c9e09cd0e3656
george.sarji <georgesarji@gmail.com>
2020-08-04 13:29:55 +0300

    Added tests and cmake execution

--- Assignments/project/main.cpp
+++ Assignments/project/main.cpp
@@ -1,12 +1,46 @@
 #include "Graph.h"
+#include "Edge.h"
+#include "Vertex.h"
+#include "GraphException.h"
 #include <iostream>
 
 using namespace mtm;
 
+void test1()
+{
+    Graph graph;
+    try
+    {
+        graph.addVertex("test");
+    }
+    catch (const mtm::GraphException &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    try
+    {
+        graph.addVertex("test1");
+    }
+    catch (const mtm::GraphException &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    graph.addEdge("test", "test1");
+    graph.addEdge("test1", "test");
+
+    try
+    {
+        graph.addEdge("test11", "test1");
+        graph.addEdge("test", "test1");
+    }
+    catch (const std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    graph.removeVertex("test");
+}
 int main()
 {
-    // Create a graph.
-    Graph graph();
-    graph.addVertex("test");
+    test1();
     return 0;
 }
