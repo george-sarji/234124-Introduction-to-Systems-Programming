462de36973be7325308c08773ce9121b312c76ec
george.sarji <georgesarji@gmail.com>
2020-08-05 21:26:17 +0300

    Added regex for numerous commands

--- Assignments/project/Calculator.cpp
+++ Assignments/project/Calculator.cpp
@@ -8,8 +8,11 @@
 #include <map>
 
 #define PROMPT "Gcalc> "
-#define SHELL_DEF "\\{\\s*[a-z,A-Z,;[\\]]\\s*(\\s*,\\s*[a-z,A-Z,;[\\]]\\s*)*\\s*(\\|\\s*(<\\s*[a-z,A-Z,;[\\]]\\s*,\\s*[a-z,A-Z,;[\\]]\\s*>)(\\s*,\\s*<\\s*[a-z,A-Z,;[\\]]\\s*,\\s*[a-z,A-Z,;[\\]]\\s*>)*)*\\}"
-
+#define SHELL_DEF "\\{\\s*[a-zA-Z;[\\]]\\s*(\\s*,\\s*[a-zA-Z;[\\]]\\s*)*\\s*(\\|\\s*(<\\s*[a-zA-Z;[\\]]\\s*,\\s*[a-zA-Z;[\\]]\\s*>)(\\s*,\\s*<\\s*[a-zA-Z;[\\]]\\s*,\\s*[a-zA-Z;[\\]]\\s*>)*)*\\}"
+#define DEFINITION "\\s*[a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*=\\s*"
+#define ADDITION "\\s*[a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*=\\s*+\\s*[a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*=\\s*"
+#define COMPLEMENT "\\s*![a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*=\\s*"
+#define PRINT "(print)\\s*(\\s*\\(\\s*)+([a-zA-Z]+[0-9]*[a-zA-Z0-9]*)(\\s*\\)\\s*)+"
 using namespace mtm;
 
 std::string toUpper(std::string str)
@@ -24,6 +27,8 @@ std::string toUpper(std::string str)
 void shellMode()
 {
     std::regex defRegexp(SHELL_DEF);
+    std::regex definitionExp(DEFINITION);
+    std::regex additionExp(ADDITION);
     // This will contain all the logic for the shell itself.
     std::cout << PROMPT;
     std::string input;
@@ -39,15 +44,17 @@ void shellMode()
             std::getline(std::cin, input);
             continue;
         }
-        if (std::regex_match(input, defRegexp))
+        if (std::regex_search(input, defRegexp))
         {
             std::cout << "Valid constructor!!!" << std::endl;
         }
-
-        else
+        if (std::regex_search(input, definitionExp))
         {
-            std::cout << "Error: Invalid command." << std::endl;
+            std::cout << "Valid defintion." << std::endl;
         }
+        std::cout << PROMPT;
+        std::getline(std::cin, input);
+        // std::cout << "Error: Invalid command." << std::endl;
     }
 }
 
@@ -59,9 +66,14 @@ int main(int argCount, char *args[])
         // SHELL
         shellMode();
     }
+    else if (argCount != 3)
+    {
+        // Error!
+        std::cout << "Error: Provide the proper syntax: gcalc input.txt output.txt" << std::endl;
+    }
     else
     {
-        // BATCH
+        // Batch mode.
         return 0;
     }
     return 1;
