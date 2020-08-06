bf38049d2dc064253fadd27ecbf3eaba9bb2a7c8
george.sarji <georgesarji@gmail.com>
2020-08-07 01:55:21 +0300

    Added basic python wrapper without error handling and without proper print

--- Assignments/project/GraphCalculator.py
+++ Assignments/project/GraphCalculator.py
@@ -1,1 +1,24 @@
 import graph as gw
+
+def create():
+    return gw.Graph()
+def destroy(graph):
+    del graph
+
+def addVertex(graph,v):
+    graph.addVertex(v)
+    return graph
+
+def addEdge(graph, v1, v2):
+    graph.addEdge(v1, v2)
+    return graph
+
+def disp(graph):
+    print.display(graph)
+
+g = create()
+g = addVertex(g, 'a')
+# g = addEdge(g, 'a', 'b')
+disp(g)
+print(g)
+destroy(g)
