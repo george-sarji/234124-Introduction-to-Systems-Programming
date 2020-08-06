7c1ab9020d877a9242966714e33ddf5a57173c9f
george.sarji <georgesarji@gmail.com>
2020-08-06 13:58:04 +0300

    Added more regex

--- Assignments/project/Calculator.cpp
+++ Assignments/project/Calculator.cpp
@@ -7,6 +7,7 @@
 #include <regex>
 #include <map>
 #include "GraphException.h"
+#include <stdlib.h>
 
 #define PROMPT "Gcalc> "
 #define SHELL_DEF "\\{\\s*[a-zA-Z;[\\]]\\s*(\\s*,\\s*[a-zA-Z;[\\]]\\s*)*\\s*(\\|\\s*(<\\s*[a-zA-Z;[\\]]\\s*,\\s*[a-zA-Z;[\\]]\\s*>)(\\s*,\\s*<\\s*[a-zA-Z;[\\]]\\s*,\\s*[a-zA-Z;[\\]]\\s*>)*)*\\}"
@@ -16,7 +17,10 @@
 #define MULTIPLICATION "\\s*[a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*\\s*\\*\\s*[a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*\\s*"
 #define INTERSECTION "\\s*[a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*\\s*\\^\\s*[a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*\\s*"
 #define COMPLEMENT "\\s*![a-zA-Z]+[0-9]*[a-zA-Z0-9]*\\s*=\\s*"
-#define PRINT "\\s*(print)\\s*(\\s*\\(\\s*)+([a-zA-Z]+[0-9]*[a-zA-Z0-9]*)(\\s*\\)\\s*)+"
+#define PRINT "\\s*(print)\\s*(\\(+\\s*.*\\s*\\)+)"
+#define RESET "\\s*(reset)\\s*"
+#define WHO "\\s*(who)\\s*"
+#define DELETE "\\s*(delete)\\(+\\s*.*\\s*\\)+\\s*"
 using namespace mtm;
 
 std::string toUpper(std::string str)
@@ -35,7 +39,9 @@ void shellMode()
     std::regex definitionExp(DEFINITION);
     std::regex additionExp(ADDITION);
     std::regex printExp(PRINT);
-
+    std::regex whoExp(WHO, std::regex_constants::icase);
+    std::regex resetExp(RESET, std::regex_constants::icase);
+    std::regex deleteExp(DELETE, std::regex_constants::icase);
     // This will contain all the logic for the shell itself.
     std::string input;
     std::map<std::string, Graph> variables;
@@ -43,7 +49,26 @@ void shellMode()
     {
         std::cout << PROMPT;
         std::getline(std::cin, input);
+        bool validCommand=false;
         // Handle the logic here.
+        if (std::regex_match(input, whoExp)) {
+            // Print out the current variables from the map.
+            for (auto it = variables.begin();it!=variables.end();++it) {
+                std::cout << it->first << std::endl;
+            }
+            continue;
+        }
+        if (std::regex_match(input, resetExp)) {
+            // Delete all the current graphs.
+            variables.clear();
+            // Clear the command line.
+            system("clear");
+            continue;
+        }
+        if (std::regex_match(input, deleteExp)) {
+
+        }
+
         Graph temp;
         std::cout << "Input:" << input << std::endl;
         if (std::regex_search(input, defRegexp))
@@ -108,6 +133,12 @@ void shellMode()
         if (std::regex_search(input, printExp))
         {
             // Print the given variable.
+            std::cout << "Valid print" << std::endl;
+            // Validate the number of parenthesis and if they're actually valid.
+        }
+
+        if (!validCommand) {
+            std::cout<<"Error: Unrecognized command '" << input << "'" << std::endl;
         }
         // std::cout << "Error: Invalid command." << std::endl;
     }
